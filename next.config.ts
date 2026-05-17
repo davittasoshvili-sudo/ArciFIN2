ac4a8b699e43a1390ce3cdf97a69a247a8f1f8ba
StartupGeorgia <info@startupgeorgia.org>
2026-05-17 12:37:22 +0400

    chore: add basePath /fin for VPS deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  basePath: '/fin',
+  assetPrefix: '/fin',
+};
 
 export default nextConfig;
